44695e54b53247bf033dc4a7ea8760668242ae68
Tyler Fost <fosty@cs.washington.edu>
2017-09-03 21:29:33 -0700

    added anthonys stuff

---
+++ Heist/app/src/main/java/brassgames/heist/util/User.java
@@ -0,0 +1,13 @@
+package brassgames.heist.util;
+
+/**
+ * Created by Anthony on 9/3/2017.
+ */
+
+public class User {
+    private String name;
+
+    public User() {
+        this.name = "Doug";
+    }
+}
